7a288b3408ef18db46a9d4e004aaf9c34c38169d
John Garcia <johngarciadev@gmail.com>
2022-10-24 20:10:32 -0700

    Added initial Courses component

---
+++ client/src/components/Courses.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+
+const Courses = () => {
+    const [courses, setCourses] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/api/courses')
+            .then( res => res.json() )
+            .then( data => setCourses(data))
+            .catch( err => console.log('Oh noes', err))
+            .finally(() => setIsLoading(false));
+    }, []);
+
+    return (
+        <div className="wrap main--grid">
+            { isLoading 
+                ?
+                <p>Loading...</p>
+                :
+                courses.map( course => (
+                    <a href={`http://localhost:5000/api/courses/${course.id}`} key={course.id}>
+                        <h2 className="course--label">Course</h2>
+                        <h3 className="course--title">{course.title}</h3>
+                    </a>
+                ))
+            }
+        </div>
+    )
+}
+
+export default Courses;
