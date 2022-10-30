d06bbd79d5957a4957580e22616ba4d1e8d0ec1b
John Garcia <johngarciadev@gmail.com>
2022-10-29 19:32:11 -0700

    Updated CourseDetail component to use URL parameter to fetch and render correct course details

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const CourseDetail = () => {
     const [courseDetails, setCourseDetails] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const { id } = useParams();
 
     useEffect(() => {
-        fetch('http://localhost:5000/api/courses/1')
+        fetch(`http://localhost:5000/api/courses/${id}`)
             .then(res => res.json())
             .then(data => setCourseDetails(data))
             .catch(err => console.log('Error fetching course details', err))
