1a1b1db3e2eaef7e651a79056a9d36a061cf88c7
John Garcia <johngarciadev@gmail.com>
2022-10-29 19:30:11 -0700

    Added home link to Header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 const Header = ()=> {
     return (
         <header>
             <div className="wrap header--flex">
                 <h1 className="header--logo">
-                    <a href="#">Courses</a>
+                    <Link to="/">Courses</Link>
                 </h1>
                 <nav>
                     <ul className="header--signedin">
