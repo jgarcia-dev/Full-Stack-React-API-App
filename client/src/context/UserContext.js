d2aad38d628110860f3b5b68ae5ed54ae45a06f4
John Garcia <johngarciadev@gmail.com>
2022-11-05 19:14:09 -0700

    Created UserContext.js to hold user global state

---
+++ client/src/context/UserContext.js
@@ -0,0 +1,35 @@
+import React, { useState, createContext } from 'react';
+import apiRequest from '../utilities/apiRequest';
+
+export const UserContext = createContext(null);
+
+const UserContextProvider = ({ children }) => {
+   
+    const [ authenticatedUser, setAuthenticatedUser ] = useState(null);
+
+    const signIn = async (username, password) => {
+        const res = await apiRequest("/users", "GET", null, true, { username, password });
+        
+        if (res.status === 200) {
+            const user = await res.json();
+            setAuthenticatedUser(user);
+        } else if (res.status === 401) {
+            setAuthenticatedUser(null);
+        } else {
+            throw new Error();
+        }
+    }
+
+    const value = {
+        authenticatedUser,
+        signIn,
+    }
+
+    return (
+        <UserContext.Provider value={value}>
+            { children }
+        </UserContext.Provider>
+    )
+}
+
+export default UserContextProvider;
