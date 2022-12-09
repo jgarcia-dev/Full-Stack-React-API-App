2beea042e00c010c3a98fe04098f791315ee49a5
John Garcia <johngarciadev@gmail.com>
2022-12-09 10:56:01 -0800

    Fixed bug that would navigate user to same page instead of previous one when using browser back button.

--- client/src/components/PrivateRoute.js
+++ client/src/components/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
+import Forbidden from './Forbidden';
 
 const PrivateRoute = () => {
     const { authenticatedUser, userMatchesCookie } = useContext(UserContext);
@@ -8,7 +9,7 @@ const PrivateRoute = () => {
     return (
         authenticatedUser && userMatchesCookie() 
             ? <Outlet /> 
-            : <Navigate to="/forbidden" />
+            : <Forbidden />
     )
 }
 
