c5067609fdc82904971086f52b73ec74832c3d19
John Garcia <johngarciadev@gmail.com>
2022-12-27 06:45:19 -0800

    Just added some comments

--- client/src/utilities/apiRequest.js
+++ client/src/utilities/apiRequest.js
@@ -1,3 +1,12 @@
+/**
+ * Makes a fetch request with parameters forming the path and options for request
+ * @param {string} path Will be appended to base path to create full path
+ * @param {string} method Defines request method used
+ * @param {object} body Data to included in body of request, defaults to null if none
+ * @param {boolean} requiresAuth Indicates if fetch request will require authorization, defaults to false
+ * @param {object} credentials User credentials for authorization, defaults to null
+ * @return {Promise} Returns results of the fetch request
+ */
 const apiRequest = (path, method, body = null, requiresAuth = false, credentials = null) => {
     const apiBaseUrl = 'http://localhost:5000/api';
     const url = apiBaseUrl + path;
